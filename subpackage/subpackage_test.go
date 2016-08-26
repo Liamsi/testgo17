a4afb41f6b4dd6143f61c4c4502551c4acbc4579
Liamsi <Liamsi@users.noreply.github.com>
2016-08-27 00:46:36 +0200

    minimal working (?) example

---
+++ subpackage/subpackage_test.go
@@ -0,0 +1,10 @@
+package subpackage
+
+import (
+	"github.com/liamsi/testgo17"
+	"testing"
+)
+
+func TestUsingSimpleType(t *testing.T) {
+	_ = testgo17.SimpleType{1, 10}	
+}
---
+++ testgo17.go
@@ -0,0 +1,6 @@
+package testgo17
+
+type SimpleType struct {
+	A int64
+	B uint64	
+}
